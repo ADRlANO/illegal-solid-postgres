144765516e59681970f298814ce82114e753b608
Adri <35582648+ADRlANO@users.noreply.github.com>
2023-05-09 21:04:39 -0300

    Styling roadblock :(

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { sql } from "@vercel/postgres";
-import { For } from "solid-js";
+import { For, Component } from "solid-js";
 import { createServerData$ } from "solid-start/server";
 import { refetchRouteData, useRouteData } from "solid-start";
 
@@ -10,10 +10,10 @@ export function routeData() {
     const { rows } = await sql`SELECT * FROM pokemon ORDER BY RANDOM() LIMIT 12`;
     rows.map((row) => ({ ...row, type: JSON.parse(row.type) }))
     return rows;
-  })
+  });
 }
 
-export default function Home() {
+function Home() {
   const pokemon = useRouteData<typeof routeData>();
   return (
     <>
@@ -25,6 +25,56 @@ export default function Home() {
         </For>
         <button onClick={() => refetchRouteData()}>Refresh</button>
       </PokemonCollection>
+    </>
+  );
+}
+
+export default function RootLayout({ children }: { children: Component }) {
+  return (
+      <>
+        <main class="flex min-h-screen flex-col items-center flex-start px-6 pt-6">
+          <h1 class="text-3xl font-bold mb-3">How is this not illegal?</h1>
+          <p class="text-center">
+            This page renders{" "}
+            <code class="py-0.5 px-1 text-sm rounded-md border border-gray-300 bg-gray-100 dark:bg-[#444] dark:border-[#666]">
+              SELECT * FROM pokemon ORDER BY RANDOM() LIMIT 12
+            </code>{" "}
+            from the edge, for every request.
+          </p>
+          <p class="mt-2 text-center">
+            What&apos;s best, the data fetching is defined directly within the
+            component tree thanks to React Server Components.{" "}
+            <a
+              href="https://twitter.com/dan_abramov/status/1341217154566402050"
+              target="_blank"
+              class="underline"
+            >
+              Legally
+            </a>
+            . (
+            <a
+              class="underline"
+              target="_blank"
+              href="https://github.com/rauchg/how-is-this-not-illegal"
+            >
+              Source
+            </a>
+            )
+          </p>
+          <Home />
+        </main>
+
+        <footer class="text-xs p-5 text-center text-gray-600">
+          Images courtesy of{" "}
+          <a
+            target="_blank"
+            class="underline"
+            href="https://github.com/PokeAPI/sprites/tree/master/sprites/pokemon"
+          >
+            PokeAPI
+          </a>{" "}
+          – Pokemon is © 1996-2023 Nintendo, Creatures, Inc., GAME FREAK
+        </footer>
     </>
   );
 }
